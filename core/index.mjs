71527fadf32bf2c3828129c9f925f42af86c665f
Matthew Blount <xkapastel@gmail.com>
2018-12-05 02:25:21 +0000

    Compacting code a bit.

--- core/index.mjs
+++ core/index.mjs
@@ -14,97 +14,111 @@
 // You should have received a copy of the GNU Affero General Public
 // License along with this program.  If not, see
 // <https://www.gnu.org/licenses/.
-
 export function stub() {
   debugger;
   throw "stub";
 }
-
 export function guard(predicate, value) {
   if (!predicate(value)) {
     debugger;
     throw "guard";
   }
 }
-
+// The Lisp API.
+//
+// Softmacs Lisp has lexically scoped fexprs, delimited continuations,
+// and syntax for keywords, vectors and maps in the style of Clojure.
+//
+// As far as data structures are concerned:
+// - Booleans, numbers and strings are just JavaScript natives.
+// - Vectors are random access lists as in Okasaki's thesis.
+// - Maps are treaps.
 export class Lisp {
-  constructor() {
-    //
-  }
-
-  get version()          { stub() }
-
-  get unit()             { stub() }
-  symbol(value)          { stub() }
-  keyword(value)         { stub() }
-  pair(fst, snd)         { stub() }
-  fst(value)             { stub() }
-  snd(value)             { stub() }
+  constructor() {}
+  // Some string associated with the version of the Lisp
+  // interpreter. You should generally use a content-address
+  // instead if you actually want some guarantees on what
+  // code is running.
+  get version() { stub() }
+  // Unit is the identity for products. It's a singleton type used in
+  // various ways, such as to signal the end of a union typed list, to
+  // avoid binding a value in a parameter list, as a return from an
+  // effectful function etc.
+  get unit() { stub() }
+  // Symbols 
+  symbol(value) { stub() }
+  // Keywords
+  keyword(value) { stub() }
+  // A pair is just two values stuck together.
+  pair(fst, snd) { stub() }
+  fst(value)     { stub() }
+  snd(value)     { stub() }
+  // A scope maps symbols to values. Softmacs Lisp
+  // is lexically scoped, so a vau expression will
+  // capture the scope at its definition site as well
+  // as binding the scope at its call site.
   init()                 { stub() }
   eval(value, scope)     { stub() }
   child(scope)           { stub() }
   set(scope, key, value) { stub() }
+  // Procedures
   primitive(body)        { stub() }
   wrap(value)            { stub() }
   unwrap(value)          { stub() }
   vau(h, t, s, d)        { stub() }
+  // Softmacs Lisp uses type predicates and assertions
+  // as in Clojure's spec.
   isUnit(value)          { stub() }
   isBoolean(value)       { stub() }
-  isSymbol(value)        { stub() }
-  isKeyword(value)       { stub() }
   isNumber(value)        { stub() }
+  isKeyword(value)       { stub() }
   isString(value)        { stub() }
+  isSymbol(value)        { stub() }
   isPair(value)          { stub() }
   isScope(value)         { stub() }
   isProcedure(value)     { stub() }
   isPrimitive(value)     { stub() }
   isApplicative(value)   { stub() }
   isOperative(value)     { stub() }
+  // Read
   read(src)              { stub() }
   show(value)            { stub() }
 }
-
-
 class Value {
-  constructor() {
-    //
-  }
+  constructor() {}
+  toString()    { stub() }
 }
-
 class Unit extends Value {
-  constructor() {
-    super();
-  }
-
-  toString() { return "#" }
+  constructor() { super() }
+  toString()    { return "#" }
 }
-
 class Symbol extends Value {
   constructor(value) {
     super();
     this.value = value;
   }
-
   toString() { return this.value }
 }
-
 class Keyword extends Value {
   constructor(value) {
     super();
     this.value = value;
   }
-
   toString() { return `:${this.value}` }
 }
-
 class Pair extends Value {
   constructor(fst, snd, isList) {
     super();
     this.fst = fst;
     this.snd = snd;
-    this.isList = isList;
+    if (isUnit(snd)) {
+      this.isList = true;
+    } else if (isPair(snd)) {
+      this.isList = snd.isList;
+    } else {
+      this.isList = false;
+    }
   }
-
   toString() {
     if (this.isList) {
       let buf = [];
@@ -124,14 +138,12 @@ class Pair extends Value {
     }
   }
 }
-
 class Scope extends Value {
   constructor(parent) {
     super();
     this.frame = new Map();
     this.parent = parent;
   }
-
   get(key) {
     if (isSymbol(key)) {
       key = key.value;
@@ -145,7 +157,6 @@ class Scope extends Value {
     }
     throw `${key} is undefined`;
   }
-
   set(key, value) {
     if (isUnit(key)) {
       return;
@@ -156,7 +167,6 @@ class Scope extends Value {
     guard(isString, key);
     this.frame.set(key, value);
   }
-
   bind(fst, snd) {
     if (isUnit(fst)) {
       return;
@@ -170,32 +180,24 @@ class Scope extends Value {
       this.bind(fst.snd, snd.snd);
     }
   }
-
   toString() { return "<scope>" }
 }
-
 class Procedure extends Value {
-  constructor() {
-    super();
-  }
-
-  toString() { return "<procedure>" }
+  constructor() { super() }
+  toString()    { return "<procedure>" }
 }
-
 class Primitive extends Procedure {
   constructor(body) {
     super();
     this.body = body;
   }
 }
-
 class Applicative extends Procedure {
   constructor(body) {
     super();
     this.body = body;
   }
 }
-
 class Operative extends Procedure {
   constructor(head, tail, lexical, dynamic) {
     super();
@@ -205,125 +207,80 @@ class Operative extends Procedure {
     this.dynamic = dynamic;
   }
 }
-
-function isUnit(value) {
-  return value instanceof Unit;
-}
-
-function isBoolean(value) {
-  return typeof(value) == "boolean";
-}
-
-function isNumber(value) {
-  return typeof(value) == "number";
-}
-
-function isString(value) {
-  return typeof(value) == "string";
-}
-
-function isSymbol(value) {
-  return value instanceof Symbol;
-}
-
-function isKeyword(value) {
-  return value instanceof Keyword;
-}
-
-function isPair(value) {
-  return value instanceof Pair;
-}
-
-function isScope(value) {
-  return value instanceof Scope;
-}
-
-function isFunction(value) {
-  return typeof(value) == "function";
-}
-
-function isPrimitive(value) {
-  return value instanceof Primitive;
-}
-
-function isApplicative(value) {
-  return value instanceof Applicative;
-}
-
-function isOperative(value) {
-  return value instanceof Operative;
-}
-
-function isProcedure(value) {
-  return value instanceof Procedure;
-}
-
+function isUnit(value)        { return value instanceof Unit        }
+function isBoolean(value)     { return typeof(value) == "boolean"   }
+function isNumber(value)      { return typeof(value) == "number"    }
+function isString(value)      { return typeof(value) == "string"    }
+function isSymbol(value)      { return value instanceof Symbol      }
+function isKeyword(value)     { return value instanceof Keyword     }
+function isPair(value)        { return value instanceof Pair        }
+function isScope(value)       { return value instanceof Scope       }
+function isFunction(value)    { return typeof(value) == "function"  }
+function isPrimitive(value)   { return value instanceof Primitive   }
+function isApplicative(value) { return value instanceof Applicative }
+function isOperative(value)   { return value instanceof Operative   }
+function isProcedure(value)   { return value instanceof Procedure   }
 class V0 extends Lisp {
   constructor() {
     super();
-    this.kUnit  = new Unit();
+    this.kUnit = new Unit();
   }
-
-  get version()  { return "0.0.0 stealth mode" }
-  get unit()     { return this.kUnit           }
-  symbol(value)  { return new Symbol(value)    }
-  keyword(value) { return new Keyword(value)   }
-
-  pair(fst, snd) {
-    let isList = false;
-    if (isUnit(snd)) {
-      isList = true;
-    } else if (isPair(snd)) {
-      isList = snd.isList;
-    } else {
-      isList = false;
-    }
-    return new Pair(fst, snd, isList);
-  }
-
+  get version()          { return "0.0.0 stealth mode"         }
+  get unit()             { return this.kUnit                   }
+  symbol(value)          { return new Symbol(value)            }
+  keyword(value)         { return new Keyword(value)           }
+  child(parent)          { return new Scope(parent)            }
+  set(scope, key, value) { return scope.set(key, value)        }
+  read(src)              { return _parse(_tokenize(src), this) }
+  isUnit(value)          { return isUnit(value)                }
+  isBoolean(value)       { return isBoolean(value)             }
+  isSymbol(value)        { return isSymbol(value)              }
+  isKeyword(value)       { return isKeyword(value)             }
+  isNumber(value)        { return isNumber(value)              }
+  isString(value)        { return isString(value)              }
+  isPair(value)          { return isPair(value)                }
+  isScope(value)         { return isScope(value)               }
+  isProcedure(value)     { return isProcedure(value)           }
+  isPrimitive(value)     { return isPrimitive(value)           }
+  isApplicative(value)   { return isApplicative(value)         }
+  isOperative(value)     { return isOperative(value)           }
+  pair(fst, snd)         { return new Pair(fst, snd)           }
   fst(value) {
     guard(isPair, value);
     return value.fst;
   }
-
   snd(value) {
     guard(isPair, value);
     return value.snd;
   }
-
   primitive(func) {
     guard(isFunction, func);
     return new Primitive(func);
   }
-
   wrap(proc) {
     guard(isProcedure, proc);
     return new Applicative(proc);
   }
-
   unwrap(proc) {
     guard(isApplicative, proc);
     return proc.body;
   }
-
   vau(head, tail, lexical, dynamic) {
     return new Operative(head, tail, lexical, dynamic);
   }
-
   eval(value, scope) {
     return this._eval(value, scope, (x) => x);
   }
-
   init() {
     let scope = new Scope();
-    scope.set("pair", this._app2(this.pair.bind(this)));
-    scope.set("fst", this._app1(this.fst.bind(this)));
-    scope.set("snd", this._app1(this.snd.bind(this)));
-    scope.set("wrap", this._app1(this.wrap.bind(this)));
-    scope.set("unwrap", this._app1(this.unwrap.bind(this)));
-    scope.set("eval", this._app2(this.eval.bind(this)));
-    scope.set("show", this._app1(this.show.bind(this)));
-    scope.set("vau", this.primitive((args, scope, rest) => {
+    scope.set("pair"   , this._app2(this.pair.bind(this)));
+    scope.set("fst"    , this._app1(this.fst.bind(this)));
+    scope.set("snd"    , this._app1(this.snd.bind(this)));
+    scope.set("wrap"   , this._app1(this.wrap.bind(this)));
+    scope.set("unwrap" , this._app1(this.unwrap.bind(this)));
+    scope.set("eval"   , this._app2(this.eval.bind(this)));
+    scope.set("show"   , this._app1(this.show.bind(this)));
+    scope.set("vau"    , this.primitive((args, scope, rest) => {
       guard(isPair, args);
       guard(isPair, args.snd);
       let proc = this.vau(args.fst, args.snd.snd, scope, args.snd.fst);
@@ -331,17 +288,6 @@ class V0 extends Lisp {
     }));
     return scope;
   }
-
-  child(parent) { return new Scope(parent) }
-
-  set(scope, key, value) {
-    return scope.set(key, value);
-  }
-
-  read(src) {
-    return _parse(_tokenize(src), this);
-  }
-
   show(value) {
     if (isBoolean(value)) {
       if (value) {
@@ -355,20 +301,6 @@ class V0 extends Lisp {
       return value.toString();
     }
   }
-
-  isUnit(value)        { return isUnit(value)        }
-  isBoolean(value)     { return isBoolean(value)     }
-  isSymbol(value)      { return isSymbol(value)      }
-  isKeyword(value)     { return isKeyword(value)     }
-  isNumber(value)      { return isNumber(value)      }
-  isString(value)      { return isString(value)      }
-  isPair(value)        { return isPair(value)        }
-  isScope(value)       { return isScope(value)       }
-  isProcedure(value)   { return isProcedure(value)   }
-  isPrimitive(value)   { return isPrimitive(value)   }
-  isApplicative(value) { return isApplicative(value) }
-  isOperative(value)   { return isOperative(value)   }
-
   _eval(value, scope, rest) {
     if (isSymbol(value)) {
       return rest(scope.get(value));
@@ -380,7 +312,6 @@ class V0 extends Lisp {
       return rest(value);
     }
   }
-
   _apply(proc, value, scope, rest) {
     guard(isProcedure, proc);
     if (isPrimitive(proc)) {
@@ -396,38 +327,31 @@ class V0 extends Lisp {
       return this._exec(proc.tail, local, rest);
     }
   }
-
   _exec(value, scope, rest) {
     if (isUnit(value)) {
       return rest(value);
-    } else if (isPair(value)) {
-      return this._eval(value.fst, scope, (fst) => {
-        return this._exec(value.snd, scope, (snd) => {
-          if (!isUnit(snd)) {
-            return rest(snd);
-          }
-          return rest(fst);
-        });
-      });
-    } else {
-      throw "exec";
     }
+    guard(isPair, value);
+    return this._eval(value.fst, scope, (fst) => {
+      return this._exec(value.snd, scope, (snd) => {
+        if (!isUnit(snd)) {
+          return rest(snd);
+        }
+        return rest(fst);
+      });
+    });
   }
-
   _evlis(value, scope, rest) {
     if (isUnit(value)) {
       return rest(value);
-    } else if (isPair(value)) {
-      return this._eval(value.fst, scope, (fst) => {
-        return this._evlis(value.snd, scope, (snd) => {
-          return rest(this.pair(fst, snd));
-        });
-      });
-    } else {
-      throw "evlis";
     }
+    guard(isPair, value);
+    return this._eval(value.fst, scope, (fst) => {
+      return this._evlis(value.snd, scope, (snd) => {
+        return rest(this.pair(fst, snd));
+      });
+    });
   }
-
   _app1(body) {
     return this.wrap(this.primitive((args, scope, rest) => {
       guard(isPair, args);
@@ -435,7 +359,6 @@ class V0 extends Lisp {
       return rest(body(args.fst));
     }));
   }
-
   _app2(body) {
     return this.wrap(this.primitive((args, scope, rest) => {
       guard(isPair, args);
@@ -444,7 +367,6 @@ class V0 extends Lisp {
       return rest(body(args.fst, args.snd.fst));
     }));
   }
-
   _appN(body) {
     return this.wrap(this.primitive((args, scope, rest) => {
       let values = arrayFromList(args);
@@ -452,15 +374,10 @@ class V0 extends Lisp {
     }));
   }
 }
-
-function isLparen(rune) {
-  return rune == "(";
-}
-
-function isRparen(rune) {
-  return rune == ")";
-}
-
+function isLparen(rune) { return rune == "(" }
+function isRparen(rune) { return rune == ")" }
+function isDot(rune)    { return rune == "." }
+function isColon(rune)  { return rune == ":" }
 function isSpace(rune) {
   switch (rune) {
   case " ":
@@ -472,19 +389,9 @@ function isSpace(rune) {
     return false;
   }
 }
-
 function isSeparator(rune) {
   return isLparen(rune) || isRparen(rune) || isSpace(rune);
 }
-
-function isDot(rune) {
-  return rune == ".";
-}
-
-function isColon(rune) {
-  return rune == ":";
-}
-
 function _tokenize(runes) {
   let index = 0;
   let tokens = [];
@@ -534,17 +441,11 @@ function _tokenize(runes) {
   }
   return tokens;
 }
-
 class Dot {
-  constructor() {
-
-  }
+  constructor() {}
 }
-
-function isPsuedo(object) {
-  return object instanceof Dot;
-}
-
+function isPsuedo(object) { return object instanceof Dot }
+function nonempty(x) { return x.length > 0 }
 function _parse(tokens, api) {
   let index = 0;
   let stack = [];
@@ -628,7 +529,6 @@ function _parse(tokens, api) {
   }
   return objects;
 }
-
 function arrayFromList(xs) {
   let buf = [];
   while (!isUnit(xs)) {
@@ -638,7 +538,4 @@ function arrayFromList(xs) {
   }
   return buf;
 }
-
-export function init() {
-  return new V0();
-}
+export function init() { return new V0() }
